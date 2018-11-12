6a5bb6616718a92853a080f894dd44695d54d4f3
Matteo Baldelli <baldelli.matteo2@gmail.com>
2018-11-12 15:15:48 +0100

    edit metrics

--- src/app/metrics/metrics.component.ts
+++ src/app/metrics/metrics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { MetricsService } from '../metrics.service';
@@ -12,10 +12,14 @@ import { Category } from '../category';
   styleUrls: ['./metrics.component.css']
 })
 export class MetricsComponent implements OnInit {
+  @ViewChild('form')
+  private templateForm: TemplateRef<any>;
   metrics: Metric[];
   categories: Category[];
   modalRef: BsModalRef;
-  newMetric = new FormGroup({
+  titleForm: string;
+  metricForm = new FormGroup({
+    id: new FormControl({ value: null, disabled: true}),
     name: new FormControl('', Validators.required),
     weight: new FormControl('', Validators.required),
     gender: new FormControl('', Validators.required),
@@ -35,38 +39,77 @@ export class MetricsComponent implements OnInit {
 
   ngOnInit() {
     this.metricsService.getMetrics().subscribe(metrics => this.metrics = metrics);
-  }
-
-  openModal(template: TemplateRef<any>) {
     this.categoryService.getCategories().subscribe(categories => {
       this.categories = categories;
-      this.modalRef = this.modalService.show(template);
     });
   }
 
   add(): void {
-    this.newMetric.disable();
-    const metric = {
-      name: this.newMetric.value.name,
-      weight: this.newMetric.value.weight,
-      gender: this.newMetric.value.gender,
-      unitLabel: this.newMetric.value.unitLabel,
-      totalRangeMin: this.newMetric.value.totalRangeMin,
-      totalRangeMax: this.newMetric.value.totalRangeMax,
-      healthyRangeMin: this.newMetric.value.healthyRangeMin,
-      healthyRangeMax: this.newMetric.value.healthyRangeMax,
-      categoryId: this.newMetric.value.categoryId
-    } as Metric;
+    this.titleForm = 'Aggiungi';
+    this.modalRef = this.modalService.show(this.templateForm, { keyboard: false });
+  }
 
+  edit(metric: Metric): void {
+    this.titleForm = 'Modifica';
+    this.metricForm.setValue({
+      id: metric.id,
+      name: metric.name,
+      weight: metric.weight,
+      gender: metric.gender,
+      unitLabel: metric.unitLabel,
+      totalRangeMin: metric.totalRangeMin,
+      totalRangeMax: metric.totalRangeMax,
+      healthyRangeMin: metric.healthyRangeMin,
+      healthyRangeMax: metric.healthyRangeMax,
+      categoryId: metric.categoryId
+    });
+    this.modalRef = this.modalService.show(this.templateForm, { keyboard: false });
+  }
+
+  onSubmit(): void {
+    this.metricForm.disable();
+    const metric = {
+      id: this.metricForm.value.id,
+      name: this.metricForm.value.name,
+      weight: this.metricForm.value.weight,
+      gender: this.metricForm.value.gender,
+      unitLabel: this.metricForm.value.unitLabel,
+      totalRangeMin: this.metricForm.value.totalRangeMin,
+      totalRangeMax: this.metricForm.value.totalRangeMax,
+      healthyRangeMin: this.metricForm.value.healthyRangeMin,
+      healthyRangeMax: this.metricForm.value.healthyRangeMax,
+      categoryId: this.metricForm.value.categoryId
+    } as Metric;
+    if (metric.id) {
+      this.onSubmitUpdate(metric);
+    } else {
+      this.onSubmitAdd(metric);
+    }
+  }
+
+  private onSubmitAdd(metric: Metric): void {
     this.metricsService.addMetric(metric).subscribe(
       newMetric => {
         this.metrics.push(newMetric);
-        this.newMetric.reset();
       },
       error => {},
       () => {
-        this.newMetric.enable();
-        this.modalRef.hide();
+        this.metricForm.enable();
+        this.hideForm();
+      }
+    );
+  }
+
+   private onSubmitUpdate(metric: Metric): void {
+    this.metricsService.updateMetric(metric).subscribe(
+      updateMetric => {
+        const index = this.metrics.findIndex(c => c.id === updateMetric.id);
+        this.metrics[index] = updateMetric;
+      },
+      error => {},
+      () => {
+        this.metricForm.enable();
+        this.hideForm();
       }
     );
   }
@@ -74,5 +117,10 @@ export class MetricsComponent implements OnInit {
   delete(metric: Metric): void {
     this.metrics = this.metrics.filter(h => h !== metric);
     this.metricsService.deleteMetric(metric).subscribe();
+  }
+
+  hideForm(): void {
+    this.metricForm.reset();
+    this.modalRef.hide();
   }
 }
