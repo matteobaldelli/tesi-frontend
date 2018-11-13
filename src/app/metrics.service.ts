64ee56578450652c8ec955f25a0737190d278e3f
Matteo Baldelli <baldelli.matteo2@gmail.com>
2018-11-13 09:41:59 +0100

    fix graph

--- src/app/metrics.service.ts
+++ src/app/metrics.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable, of, throwError} from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { environment } from '../environments/environment';
@@ -32,8 +32,8 @@ export class MetricsService {
     );
   }
 
-  getMetrics(): Observable<Metric[]> {
-    return this.http.get<Metric[]>(this.url).pipe(
+  getMetrics(params: HttpParams): Observable<Metric[]> {
+    return this.http.get<Metric[]>(this.url, {params: params}).pipe(
       tap(metrics => this.log('fetched metrics')),
       catchError(this.handleError('getMetrics', []))
     );
