5207f30f9f8efc442b09d2e3bb4e8ea720e9342b
Matteo Baldelli <baldelli.matteo2@gmail.com>
2018-12-13 15:04:41 +0100

    add more language

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/frontend-tesi'));
 
-app.get('/*', function(req,res) {
+app.get('*', (req, res) => {
+	//this is for i18n
+  const supportedLocales = ['en', 'it'];
+  const defaultLocale = 'en';
+  const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
+  //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+  const locale = (matches && supportedLocales.indexOf(matches[1]) !== -1) ? matches[1] : defaultLocale;
 
-res.sendFile(path.join(__dirname,'/dist/frontend-tesi/index.html'));
+  // res.render("${locale}/index", {req});
+  res.sendFile(path.join(__dirname, '/dist/frontend-tesi/' + locale + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
+import { Component, AfterViewChecked, ChangeDetectorRef, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { PlatformLocation } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { setTheme } from 'ngx-bootstrap/utils';
 import { UserService } from './user.service';
 
@@ -7,15 +10,28 @@ import { UserService } from './user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewChecked{
+export class AppComponent implements OnInit, AfterViewChecked{
   navbarOpen = false;
   logged: boolean;
+  currentUrl: string;
+  baseHref: string;
 
   constructor(
     private cdRef: ChangeDetectorRef,
+    private router: Router,
     public userService: UserService,
+    private  platformLocation: PlatformLocation
   ) {
     setTheme('bs4');
+  }
+
+  ngOnInit(): void {
+    this.baseHref = this.platformLocation.getBaseHrefFromDOM();
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+     .subscribe((event: NavigationEnd) => {
+       this.currentUrl = this.router.url;
+     });
   }
 
   ngAfterViewChecked() {
